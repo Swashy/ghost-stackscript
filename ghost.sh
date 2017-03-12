eb64d9898687d2de840ef2cc86a11d0e7becd9d1
Swashy OSX <roland.macdavid@gmail.com>
2017-03-11 22:08:17 -0500

    if statements for nginx SSL blocks, fix up ssh and distro verification conditionals

--- ghost.sh
+++ ghost.sh
@@ -14,8 +14,19 @@
 #WEBSITE=
 #<UDF name="pubkey" Label="Enter your public key here" default="">
 #PUBKEY=
+#<UDF name="SSL" Label="HTTPS?" oneOf="Yes,No" default="No">
+#SSL=
+#<UDF name="EMAIL" Label="Email address for the Let's Encrypt Certificate?" default="">
+#EMAIL
 ##
 # Force IPv4 because Ubuntu has a security repo with IPv6 that's been broken for a few years
+
+PROPERVERSION=$(lsb_release -a 2>/dev/null | grep 16.04)
+if [ -z "$PROPERVERSION" ]; then
+   echo "Your distribution is not supported by this StackScript"
+   exit
+fi
+
 yes | apt-get -o Acquire::ForceIPv4=true update
 yes | apt-get -o Acquire::ForceIPv4=true install vim nginx zip build-essential nodejs npm
 
@@ -23,27 +34,48 @@ set -x
 
 #If $PUBKEY is empty, then...
 if [ -z "$PUBKEY" ] || [ -n "$PUBKEY" ] || [ ! "$PUBKEY" ]; then
+  #My ssh_config file tries key auth first in 7 different ways.
+  #If there is no pubkey, then passwordauth fails for me.
+  echo "MaxAuthTries 7" >> /etc/ssh/sshd_config
+  systemctl restart sshd
+else
   echo "setting pubkey and disabling password auth..."
   mkdir -p /root/.ssh/
   touch /root/.ssh/authorized_keys
   echo "$PUBKEY" >> /root/.ssh/authorized_keys
   sed -i.bak "/PasswordAuthentication/ s/yes/no/" /etc/ssh/sshd_config
-  sed -i.bak "/PasswordAuthentication/ s/#//" /etc/ssh/sshd_config
-  systemctl restart sshd
-else
-  #My ssh_config file tries key auth first in 7 different ways.
-  #If there is no pubkey, then passwordauth fails for me.
-  echo "MaxAuthTries 7" >> /etc/ssh/sshd_config
+  sed -i.bak "/#PasswordAuthentication/ s/#PasswordAuthentication/PasswordAuthentication/" /etc/ssh/sshd_config
   systemctl restart sshd
 fi
 
 touch /etc/nginx/sites-available/ghost.conf
 export ipaddress=$(curl ipv4.icanhazip.com)
 
-echo -e "server {
-    listen 80 default_server;
+if [ $SSL == "Yes"]; then
+    echo -e "server {
+    listen 443 default_server;
     listen [::]:80 default_server;
     server_name _;
+    location ~ /.well-known {
+      root /srv/ghost/letsencrypt;
+      allow all;
+    }
+    ssl_certificate     /etc/letsencrypt/live/$WEBSITE/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/$WEBSITE/privkey.pem;
+    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
+    ssl_ecdh_curve secp384r1; # Requires nginx >= 1.1.0
+    ssl_session_cache shared:SSL:10m;
+    ssl_session_tickets off; # Requires nginx >= 1.5.9
+    ssl_stapling on; # Requires nginx >= 1.3.7
+    ssl_stapling_verify on; # Requires nginx => 1.3.7
+    resolver 8.8.8.8 8.8.4.4 valid=300s;
+    resolver_timeout 5s;
+    add_header Strict-Transport-Security \"max-age=63072000; includeSubdomains\";
+    add_header X-Frame-Options DENY;
+    add_header X-Content-Type-Options nosniff;
+    ssl_dhparam /etc/ssl/certs/dhparam.pem;
 
     location / {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -52,14 +84,41 @@ echo -e "server {
         proxy_pass http://127.0.0.1:2368;
     }
 }" > /etc/nginx/sites-available/ghost.conf
-ln -s /etc/nginx/sites-available/ghost.conf /etc/nginx/sites-enabled/ghost.conf
-sed -i.bak '/default_server/d' /etc/nginx/sites-available/default
+
+  ln -s /etc/nginx/sites-available/ghost.conf /etc/nginx/sites-enabled/ghost.conf
+  sed -i.bak '/default_server/d' /etc/nginx/sites-available/default
+
+  ##### Let's Encrypt Certificate generation #######
+  openssl dhparam -dsaparam -out /etc/ssl/certs/dhparam.pem 2048
+  mkdir -p /srv/ghost/
+  mkdir /srv/ghost/letsencrypt
+  useradd ghost
+  chown -R ghost:ghost /srv/ghost/
+
+  echo 'deb http://ftp.debian.org/debian jessie-backports main' | tee /etc/apt/sources.list.d/backports.list
+  apt-get update
+  yes | apt-get install certbot -t jessie-backports -y
+  certbot --dry-run -m $EMAIL --agree-tos certonly -a webroot --webroot-path=/srv/ghost/letsencrypt -d $WEBSITE -d www.$WEBSITE
+else
+  echo -e "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    location / {
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header Host \$http_host;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_pass http://127.0.0.1:2368;
+    }
+}" > /etc/nginx/sites-available/ghost.conf
+  ln -s /etc/nginx/sites-available/ghost.conf /etc/nginx/sites-enabled/ghost.conf
+  sed -i.bak '/default_server/d' /etc/nginx/sites-available/default
+fi
+
 
 #Set xtrace output for debugging and so we can see commands as they're running in Lish
 #Reference http://wiki.bash-hackers.org/scripting/debuggingtips#use_shell_debug_output
 
-mkdir -p /srv/ghost/
-useradd ghost
 mkdir -p /home/ghost
 chown -R ghost:ghost /home/ghost
 cd /srv/
@@ -95,5 +154,6 @@ echo ""
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 echo "Ghost installation complete! You may now visit your IP or domain if /
 you've already configured it to get started. Admin interface is going to be/
-http://$ipaddress/ghost/"
+http://$ipaddress/ghost/ or https://$WEBSITE if you used a resolving domain/
+ with HTTPS for this script."
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
